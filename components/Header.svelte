d6842a7a9915d98df46c7113d538a6291d9ff0c4
petterkoding <69626923+petterkoding@users.noreply.github.com>
2024-05-10 19:44:33 +0200

    update index and Header

---
+++ components/Header.svelte
@@ -0,0 +1,61 @@
+<script>
+</script>
+
+<header>
+	<a class="sitename" href="/">Mombasa station</a>
+
+	<nav>
+		<ul>
+			<li>
+				<a href="/">home</a>
+			</li>
+			<li>
+				<a href="/leaderboards">leaderboards</a>
+			</li>
+			<li>
+				<a href="/weapons">weapons</a>
+			</li>
+			<li>
+				<a href="/maps">maps</a>
+			</li>
+		</ul>
+	</nav>	
+</header>
+
+<style>
+
+	header {
+		width: 100%;
+		height: 90px;
+		background-color: rgba(0, 0, 0, 0.808);
+		position: fixed;
+		z-index: 999;
+		display: flex;
+		align-items: center;
+		padding: 2rem;
+	}
+
+	nav ul {
+		display: flex;
+		align-items: center;
+		gap: 3rem;
+		
+	}
+
+	li a {
+		font-size: 1.2rem;
+		color: white;
+		text-transform: uppercase;
+		text-decoration: none;
+	}
+
+	.sitename {
+		font-size: 1.35rem;
+		color: white;
+		text-decoration: none;
+		text-transform: uppercase;
+		margin-right: 5rem;
+		font-weight: bold;
+	}
+	
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,19 @@
 <script>
 	export let data
-	console.log(data);
+	// console.log(data);
 </script>
 
 <svelte:head>
 	<title>Halo</title>
-	<meta name="description" content="Hallloo" />
+	<meta name="description" content="Mombasa of Halo" />
 </svelte:head>
 
 <section>
-	<h1>Halo 5</h1>
-	<p>Join the Banished. In Operation: Banished Honor, liberate yourself through the teachings of Atriox and claim your spoils. The free 20-tier Operation Pass is full of powerful customization rewards to overcome your foes. Upgrade to the Premium Pass to obtain the Legendary Armor Coating, Fuego Flame, and strike fear into the hearts of your opponents. The Premium Pass comes with an additional Challenge Slot, bonus XP, and never expires.</p>
-	<button>JOIN NOW</button>
-
+	<div class="overlay-text">		
+		<h1>Halo 5</h1>
+		<p>Mombasa Station is the place where you'll be able to find the top scorers in H5 leaderboards, track down your favorite weapon or just reminiscence the good 'ol RvB fights from back in the day.</p>
+		<button>JOIN NOW</button>
+	</div>
 </section>
 
 <!-- {#each data.weapons as weapon}
@@ -22,28 +23,58 @@
 
 <style>
 
-	section{
-		background-image: url(Halo-Banished.avif);
+	.overlay-text {
+		position: absolute; 
+		z-index: 100;
+		margin-left: 10%;
+	}
+
+	h1 {
+		color: white;
+		font-size: 2rem;
+  		font-weight: 600;
+	}
+
+	p {
+		color: var(--text-faded);
+		font-size: 0.95rem;
+		max-width: 700px;
+		margin: 1rem 0;
+		line-height: 1.5;
+	}
+
+	button {
+		background: white;
+		padding: 1rem 2rem;
+		font-size: var(--font-size-button);
+		border: none;
+	}
+
+	section {
+		background-image: url(https://wpassets.halowaypoint.com/wp-content/2024/04/vNext-MBI-Infinite-Hero-Desktop-S7-R01-scaled.jpg);
 		background-position: center;
 		background-repeat: no-repeat;
 		background-size: cover;
 		width: 100%;
-		height: 1000px;
+		height: 100dvh;
 		position: relative;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-start;
 
 		&:after{
 			content: "";
-			width: 100%;
+			width: 80%;
 			position: absolute;
 			left: 0;
 			top: 0;
 			height: 100%;
 
 			background: rgb(0,0,0);
-			background: linear-gradient(90deg, rgba(0,0,0,1) 3%, rgba(0,0,0,0.1386204139859069) 40%, rgba(255,255,255,0) 100%);
+			background: linear-gradient(90deg, rgba(0,0,0,0.8547794117647058) 5%, rgba(0,0,0,0.8323704481792717) 30%, rgba(255,255,255,0) 70%); 
 
-		}
-
-		
+		}		
 	}
 </style>
